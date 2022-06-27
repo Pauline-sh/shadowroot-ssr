61f04000e6aeb53536c214e67a0e5b0fe8b77df2
Polina Shestakova <paulin-sh@yandex.ru>
2022-06-28 00:33:14 +0300

    fix module resolution

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,26 +1,18 @@
+import path from 'path';
 import express from 'express';
 import logger from 'morgan';
-import path from 'path';
-const helmetImport = import('helmet');
+import helmet from 'helmet';
+import {router} from './router/routes/index.js';
 
-export const getApp = async () => {
-  const helmet = (await helmetImport).default;
-  const app = express();
+export const app = express();
 
-  app.use(logger('dev'));
-  app.use(helmet());
-  app.use(helmet.contentSecurityPolicy({
-    directives: {
-      'script-src': ["'self'"],
-      'style-src': ["'self'"],
-    },
-  }));
-
-  app.use(express.static(path.join('src', 'client')));
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src', 'client', 'index.html'));
-  });
-
-  return app;
-};
+app.use(logger('dev'));
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    'script-src': ["'self'"],
+    'style-src': ["'self'"],
+  },
+}));
+app.use(express.static(path.join('src', 'client')));
+app.use(router);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,6 @@
-import {PORT} from './env';
-import {getApp} from './app';
+import {PORT} from './env.js';
+import {app} from './app.js';
 
-(async function main() {
-  const app = await getApp();
-
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-  });
-})();
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
